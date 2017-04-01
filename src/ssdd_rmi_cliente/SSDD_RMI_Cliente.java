d85e66b69e7974e9dea3d9c21cadc95c0a867ae8
jprecreativo <jprecreativo@gmail.com>
2017-04-01 15:06:57 +0200

    Creada la pantalla inicial con el logo de la DGT. Aún no he podido conectar al cliente con el servidor.

--- src/ssdd_rmi_cliente/SSDD_RMI_Cliente.java
+++ src/ssdd_rmi_cliente/SSDD_RMI_Cliente.java
@@ -1,9 +1,18 @@
 package ssdd_rmi_cliente;
 
+import interfaces.PantallaInicial;
+import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+
 public class SSDD_RMI_Cliente 
 {
-    public static void main(String[] args) 
+    public static void main(String[] args) throws NotBoundException, MalformedURLException, RemoteException 
     {
+        PantallaInicial inicial = new PantallaInicial();
+        InterfazMultas stub = (InterfazMultas) Naming.lookup("rmi://localhost:12345/Multas");
         
+        inicial.setVisible(true);
     }
 }
